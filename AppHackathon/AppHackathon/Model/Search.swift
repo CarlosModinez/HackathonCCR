63ba47116b2ca6a93a6980e8b1ce8ade8835542b
José Bestel <josegbestel@gmail.com>
2020-06-14 12:46:17 -0300

    This commit partially implement Model and add imgs on the assets

---
+++ AppHackathon/AppHackathon/Model/Search.swift
@@ -0,0 +1,13 @@
+//
+//  Search.swift
+//  AppHackathon
+//
+//  Created by José Guilherme Bestel on 14/06/20.
+//  Copyright © 2020 José Guilherme Bestel. All rights reserved.
+//
+
+import Foundation
+
+class Search{
+    
+}
