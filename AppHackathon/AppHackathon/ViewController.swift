0fb8d6780cd813964c0751c5812edff22ed10e1a
José Bestel <josegbestel@gmail.com>
2020-06-13 21:31:19 -0300

    This commit create the xcode proj

---
+++ AppHackathon/AppHackathon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppHackathon
+//
+//  Created by José Guilherme Bestel on 13/06/20.
+//  Copyright © 2020 José Guilherme Bestel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
